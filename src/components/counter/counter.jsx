163230e95367c1760a64d8e0844f94af73906283
Farnoosh Johnson <fjohnson@urban.org>
2021-08-31 13:56:25 -0400

    Clean our code for other practices.

--- src/components/counter/counter.jsx
+++ src/components/counter/counter.jsx
@@ -2,24 +2,13 @@ import React, { Component } from "react";
 import { Badge, Button } from "react-bootstrap";
 export default class Counter extends Component {
   state = {
-    count: 1,
-    imageUrl: "https://picsum.photos/200",
+    count: 0,
     tags: ["tag1", "tag2", "tag3"],
   };
 
   render() {
-    let classes = this.getBadgeClasses();
-    const bgColor = this.state.count === 0 ? "warning" : "primary";
-
     return (
       <React.Fragment>
-        {/* <img src={this.state.imageUrl} alt="" /> */}
-        <Badge bg={bgColor} className={this.getBadgeClasses()}>
-          {this.formatCount()}
-        </Badge>
-        <Button variant="primary" size="sm">
-          Increment
-        </Button>
         <ul>
           {this.state.tags.map((tag) => (
             <li key={tag}> {tag} </li>
@@ -27,16 +16,5 @@ export default class Counter extends Component {
         </ul>
       </React.Fragment>
     );
-  }
-
-  getBadgeClasses() {
-    let classes = "m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
-    return classes;
-  }
-
-  formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
   }
 }
