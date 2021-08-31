f197349a7f1117e5343364127e71dfb32fcc7209
Farnoosh Johnson <fjohnson@urban.org>
2021-08-31 15:02:50 -0400

    Updating the state. it will bring the dom in-sync with virtual dom.

--- src/components/counter/counter.jsx
+++ src/components/counter/counter.jsx
@@ -6,18 +6,14 @@ export default class Counter extends Component {
     tags: ["tag1", "tag2", "tag3"],
   };
 
-  // constructor() {
-  //   super();
-  //   this.handleIncrement = this.handleIncrement.bind(this);
-  // }
-
   renderTags() {
     if (this.state.tags.length === 0) return <p> There are no Tags!</p>;
     return this.state.tags.map((tag) => <li key={tag}>{tag}</li>);
   }
 
   handleIncrement = () => {
-    console.log("Increment Clicked", this);
+    this.setState({ count: this.state.count + 1 });
+    console.log(this.state.count);
   };
 
   render() {
