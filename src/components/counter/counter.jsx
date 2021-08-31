a2f6293ecb682786aa4e10d3df2708076da23a29
Farnoosh Johnson <fjohnson@urban.org>
2021-08-31 13:55:17 -0400

    Map through an array.

--- src/components/counter/counter.jsx
+++ src/components/counter/counter.jsx
@@ -4,6 +4,7 @@ export default class Counter extends Component {
   state = {
     count: 1,
     imageUrl: "https://picsum.photos/200",
+    tags: ["tag1", "tag2", "tag3"],
   };
 
   render() {
@@ -19,6 +20,11 @@ export default class Counter extends Component {
         <Button variant="primary" size="sm">
           Increment
         </Button>
+        <ul>
+          {this.state.tags.map((tag) => (
+            <li key={tag}> {tag} </li>
+          ))}
+        </ul>
       </React.Fragment>
     );
   }
