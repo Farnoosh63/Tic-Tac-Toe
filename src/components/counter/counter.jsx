d8cb0415259309af3cec4741a201cdba19a004f1
Farnoosh Johnson <fjohnson@urban.org>
2021-08-31 14:34:23 -0400

    refactor with renderTags,

--- src/components/counter/counter.jsx
+++ src/components/counter/counter.jsx
@@ -6,14 +6,15 @@ export default class Counter extends Component {
     tags: ["tag1", "tag2", "tag3"],
   };
 
+  renderTags() {
+    if (this.state.tags.length === 0) return <p> There are no Tags!</p>;
+    return this.state.tags.map((tag) => <li key={tag}>{tag}</li>);
+  }
+
   render() {
     return (
       <React.Fragment>
-        <ul>
-          {this.state.tags.map((tag) => (
-            <li key={tag}> {tag} </li>
-          ))}
-        </ul>
+        <ul>{this.renderTags()}</ul>
       </React.Fragment>
     );
   }
